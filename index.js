35e88d57114778fa99b461e83fcfa40ecebd3f31
Vedant Pawar <101568818+vedantpawar18@users.noreply.github.com>
2022-09-20 09:27:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const googleStrategy= require("passport-google-oauth20");
 const app=express();
 
 passport.use(new googleStrategy({
-    clientID:"313822891691-5ln4h6be1952kc9q21eg8pvi5c5scg60.apps.googleusercontent.com",
-    clientSecret:"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",
+    clientID:"",
+    clientSecret:"",
     callbackURL:"/auth/google/callback"
 },(accessToken, refreshToken, profile, done)=>{
     console.log(accessToken)
